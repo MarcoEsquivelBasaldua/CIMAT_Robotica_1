793da91fd8263f646873c56c434714397838d296
MarcoEsquivelBasaldua <marco.esquivel@cimat.mx>
2021-11-22 20:31:33 -0600

    rrt

---
+++ RRT_star/README.md
@@ -0,0 +1,7 @@
+# Implementacion del algoritmo RRT\*
+
+Entre los algoritmos para planificaci'on de trayectorias en rob'otica, el algoritmo *Rapidly-Exploring-Random Tree (RRt)* genera un grafo y una trayectoria dentro de este conectado un punto inicial con un punto meta mediante nodos y aristas, esta trayectoria no es necesariamente 'optima. El algoritmo *RRT\** es una modificaci'on optimizada del *RRT* que encuentra la tryectoria m'as corta entre dos puntos conforme se realiza la exploraci'on.<br />
+
+En este trabajo se hace uso del modulo de *Pygame* de *Python* para conectar dos puntos en un ambiente de dos dimensiones. Las instruciones se despliegan al ejecutar el programa (cualquiera de los dos).<br />
+
+Para mayor informaci'on consultar el archivo PDF adjunto.
---
+++ RRT_star/rrt_star_optimized.py
@@ -0,0 +1,209 @@
+import pygame
+import time
+import numpy as np
+import random
+
+
+# Colors
+white = (255,255,255)
+black = (0,0,0)
+purple = (196,18,211)
+light_green = (100,200,0)
+darker_green = (0,130,0)
+dark_blue = (0,80,165)
+light_blue = (0,0,255)
+red = (255,0,0)
+
+
+## Initialize pygame
+pygame.init()
+pygame.display.set_caption('RRT*')
+
+Length = 800
+Width = 600
+screen = pygame.display.set_mode((Length,Width))
+screen.fill(white)
+
+
+# Euclidean distance function
+def dist(x,y):
+    return np.sqrt((y[0]-x[0])**2 + (y[1]-x[1])**2)
+
+
+# Collition check function
+def coll_check(x1, x2):
+    ratio = 0.0
+    samples = 10
+
+    for i in range(1,samples):
+        ratio += 1.0/samples 
+        x = x1[0] + int(ratio*(x2[0] - x1[0]))
+        y = x1[1] + int(ratio*(x2[1] - x1[1]))
+
+        if screen.get_at((x, y)) == black:
+            return True
+
+    return False
+
+
+
+# RRT* algorithm
+def RRT_star(x_init, x_goal, step_max, max_neighborhood, n, epsilon):
+    reached_goal = False
+    T = []
+
+    x_new = []
+    x_new.append(x_init)
+    x_new.append(0.0)
+    x_new.append(0)
+
+    T.append(x_new)
+
+    for i in range(0,n):
+        x = random.randint(0,Length - 1)
+        y = random.randint(0,Width - 1)
+        x_rand = np.array([x, y])
+
+        # find nearest node
+        d_node = float('inf')
+        for k in range(len(T)):
+            if dist(x_rand, T[k][0]) < d_node:
+                x_near = k
+                d_node = dist(x_rand, T[k][0])
+        
+        # Constrain edge to step_max
+        d_near = dist(x_rand, T[x_near][0])
+        if d_near > step_max:
+            ratio = step_max/d_near
+            x = T[x_near][0][0] + int(ratio*(x_rand[0] - T[x_near][0][0]))
+            y = T[x_near][0][1] + int(ratio*(x_rand[1] - T[x_near][0][1]))
+            x_rand = np.array([x, y])
+            
+        
+        # Check if x_rand is in free space
+        if screen.get_at(x_rand) != black:
+            
+            # Look for nodes in neigborhood
+            Neighborhood = []
+            for k in range(len(T)):
+                if dist(x_rand, T[k][0]) < max_neighborhood:
+                    node_Neighborhhod = []
+                    node_Neighborhhod.append(T[k])
+                    node_Neighborhhod.append(k)
+
+                    Neighborhood.append(node_Neighborhhod)
+            
+            # Order Neighborhood by cost
+            Neighborhood.sort(key = lambda t: t[1])
+
+            # Find nearest node by cost
+            for k in Neighborhood:
+                if coll_check(k[0][0], x_rand) == False:
+                    x_new = []
+                    x_new.append(x_rand)
+                    d = dist(x_rand, k[0][0])
+                    x_new.append(d + k[0][1])
+                    x_new.append(k[1])
+
+                    T.append(x_new)
+
+                    if dist(x_rand, x_goal) < epsilon: # If the goal is reached
+                        reached_goal = True
+                    
+                    ## Rewire
+                    for k in Neighborhood:
+                        d = dist(x_rand, k[0][0])
+                        if x_new[1] + d < k[0][1] and coll_check(x_rand, k[0][0]) == False:
+                            k[0][2] = len(T) - 1
+                            k[0][1] = x_new[1] + d
+                    ######
+                    break
+    
+    return T, reached_goal, i
+
+
+
+# game loop
+running = True
+state = 0
+while running:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if pygame.mouse.get_pressed()[0] and state == 0:
+            mouse_pos = pygame.mouse.get_pos()
+            pygame.draw.circle(screen, black, mouse_pos, 10)
+
+        if pygame.mouse.get_pressed()[0] and 0 < state < 100:
+            mouse_pos = pygame.mouse.get_pos()
+            pygame.draw.circle(screen, purple, mouse_pos, 8)
+
+            x_init = np.asarray(mouse_pos)
+
+            state = 101
+        if pygame.mouse.get_pressed()[2] and 100 < state < 200:
+            mouse_pos = pygame.mouse.get_pos()
+            pygame.draw.circle(screen, red, mouse_pos, 20)
+
+            x_goal = np.asarray(mouse_pos)
+
+            state = 300
+            pygame.display.update()
+        if pygame.key.get_pressed()[13] == 1:
+            state += 1
+
+    if state > 200:
+        start_time = time.time()
+        T, goal_reached, nodes = RRT_star(x_init,x_goal, 20.0, 60.0, 10000, 30.0)
+        elapsed_time = time.time() - start_time
+
+        print('Time needed to biuld the Tree ', elapsed_time, ' seconds')
+        state = -1
+
+        # Plot tree
+        node_radius = 5
+        line_wide = 1
+        for k in range(1,len(T)):
+            pygame.draw.line(screen, light_green, T[k][0], T[T[k][2]][0], line_wide)
+            pygame.draw.circle(screen, darker_green, T[k][0], node_radius)
+        
+        pygame.draw.circle(screen, purple, x_init, 8)     # Draw x_init and x_goal point again
+        pygame.draw.circle(screen, red, mouse_pos, 20)
+        pygame.display.update()
+
+        # Plot trayectorie fron x_init to x_goal
+        if goal_reached:
+            # Find closest node with cheapest cost
+            d_max = 30.0
+            last_cost = float('inf')
+
+            for k in range(0, len(T)):
+                if dist(x_goal, T[k][0]) < d_max and T[k][1] < last_cost:
+                    index = k
+                    d_max = dist(x_goal, T[k][0])
+                    last_cost = T[k][1]
+
+
+            print('Goal Reached')
+            print('Cost to the goal ', T[index][1])
+            print('Nodes expantion ', nodes+1)
+
+            #index = T[-1][2]
+            node = T[index][0]
+            pygame.draw.line(screen, light_blue, x_goal, node, line_wide)
+            pygame.draw.circle(screen, dark_blue, node, node_radius)
+
+            index = T[index][2]
+            while index != 0:
+                pygame.draw.line(screen, light_blue, node, T[index][0], line_wide)
+                pygame.draw.circle(screen, dark_blue, T[index][0], node_radius)
+
+                node = T[index][0]
+                index = T[index][2]
+            
+            pygame.draw.line(screen, light_blue, node, x_init, line_wide)
+        else:
+            print('Goal NOT Reached')
+
+    pygame.display.update()
